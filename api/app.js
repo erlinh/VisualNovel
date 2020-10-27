16544976825f114f3544472eacf7e4aba1d9fcd6
rohbdn07 <rohbdn07@users.noreply.github.com>
2020-10-27 16:47:36 +0200

    Connected to database

--- api/app.js
+++ api/app.js
@@ -1,13 +1,31 @@
+
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
+const mangoose = require("mongoose");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//connect to Mangodb...
+const dbURI = process.env.mongodb_URI;
+mangoose.connect( dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log("there is an error", err));
+//listing to LocalHost
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- api/routes/index.js
+++ api/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.send("ok");
 });
 
+router.get('/story',(req,res)=>{
+  res.send('this is story section')
+})
 module.exports = router;
