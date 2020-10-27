0486af8660878e4f7421a9006e952d3deb1794f1
bea94k <beatakusnierz94@gmail.com>
2020-10-27 13:22:37 +0200

    Add sample json data, basic browsing grid, story cards render sample data through props

--- client/src/components/BrowsingGrid/BrowsingGrid.js
+++ client/src/components/BrowsingGrid/BrowsingGrid.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import StoryCard from '../StoryCard/StoryCard';
+import StoryCards from './StoryCards/StoryCards';
+
+import './BrowsingGrid.css';
+
+// import {Col, Row} from 'reactstrap';
 
 export default function BrowsingGrid() {
   return (
-    <div>
-      <StoryCard />
-      <StoryCard />
-      <StoryCard />
-      <StoryCard />
+    <div className="browsing-grid-wrapper">
+      <StoryCards/>
     </div>
   );
 }
+
+{/* <Row>
+  <Col xs="3"><StoryCard /></Col>
+  <Col xs="3"><StoryCard /></Col>
+  <Col xs="3"><StoryCard /></Col>
+  <Col xs="3"><StoryCard /></Col>
+</Row>
+<Row>
+  <Col xs="3"><StoryCard /></Col>
+  <Col xs="3"><StoryCard /></Col>
+  <Col xs="3"><StoryCard /></Col>
+  <Col xs="3"><StoryCard /></Col>
+</Row> */}
--- client/src/components/StoryCard/StoryCard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function StoryCard() {
-  return (
-    <div>
-      <p>This is a story card.</p>
-    </div>
-  );
-}
