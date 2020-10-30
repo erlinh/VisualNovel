41d3398d4f4661c7df34df37f1c7dd9d44166272
rohbdn07 <rohbdn07@users.noreply.github.com>
2020-10-30 15:54:05 +0200

    Styling the details page

--- client/src/components/Details/TopSection/TopCard.js
+++ client/src/components/Details/TopSection/TopCard.js
@@ -4,25 +4,26 @@ import '../TopSection/TopCard.css'
 const TopCard =()=>{
     return(
         <>
-        <div className="media topSection__bg text-light p-5 ">
+        <div className="media topSection__bg">
+            <div className="container d-flex py-5 text-light">
             <img src="https://via.placeholder.com/200x350" alt="img"/>
-            <div class="media-body pl-5">
-                <h5 class="mt-0">Title of the Book</h5>
+            <div className="container  media-body pl-5">
+                <h5 className="mt-0">Title of the Book</h5>
                 <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
                 </p>
-                <h6 class="mt-0">Author</h6>
+                <h6 className="mt-0">Author</h6>
                 <p> Cras sit amet nibh libero.
                 </p>
-                <h6 class="mt-0">Description</h6>
+                <h6 className="mt-0">Description</h6>
                 <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
                     Cras purus odio, vestibulum in vulputate at, 
                     tempus viverra turpis. 
                     Fusce condimentum nunc ac nisi vulputate fringilla. 
                 </p>
-                <h6 class="mt-0">Categories</h6>
+                <h6 className="mt-0">Categories</h6>
                 <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
                 </p>
-                <h6 class="mt-0 pb-3"> rating: 4.5 </h6>
+                <h6 className="mt-0 pb-3"> rating: 4.5 </h6>
                 <h6 class="mt-0 pb-3"> IMBD: <i className="fas fa-star" style={{color:"yellow"}}></i> <i className="fas fa-star" style={{color:"yellow"}}>
 
                 </i> <i className="fas fa-star" style={{color:"yellow"}}></i></h6>
@@ -32,6 +33,7 @@ const TopCard =()=>{
                 <ButtonBuy />
                 
             </div>
+        </div>
         </div>
        
         </>
