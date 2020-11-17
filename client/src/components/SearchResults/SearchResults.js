638cd82b2559cb99cd0262397ae86788ef1092d9
bea94k <beatakusnierz94@gmail.com>
2020-11-17 11:36:24 +0200

    Display of search results

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Link} from 'react-router-dom';
+import { Button } from 'reactstrap';
+
+import './SearchResults.css';
 
 export default function SearchResults({searchTerm, searchResults}) {
     // console.log(searchResults);
 
     const storiesToDisplay = searchResults.map(story => {
         return (
-        <div key={story.slug}>
-            id={story.slug} author={story.author} title={story.title}
+        <div key={story.slug} className="search-res-card mb-2 d-flex">
+            <img src="https://picsum.photos/150" alt="img" />
+            <div className="pl-3">
+            <h3>{story.title}</h3>
+            <p>By {story.author}</p>
+            <p>Rating: {story.rating}/5</p>
+            <Link to= {`/stories/${story.slug}`}>
+                <Button color="primary">Read More</Button>
+            </Link>
+            </div>
         </div>)
     })
 
