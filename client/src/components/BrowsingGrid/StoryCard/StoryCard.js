328eaa62e4ce72ff46d3429c71ed25598d8ffe61
bea94k <beatakusnierz94@gmail.com>
2020-11-18 16:55:39 +0200

    Change the hard-coded categories in frontend to display the correct categories

--- client/src/components/BrowsingGrid/StoryCard/StoryCard.js
+++ client/src/components/BrowsingGrid/StoryCard/StoryCard.js
@@ -14,13 +14,13 @@ const StoryCard=({id, title, author, imgUrl, rating, slug, categories})=> {
       <img className="pb-3" src={imgUrl} alt="Cover of the story" />
       <CardTitle style={{fontSize:'2rem'}}>{title}</CardTitle>
       <CardSubtitle>By {author}</CardSubtitle>
-      <p>This is a story card.</p>
-      <p>ID: {id}</p>
+      {/* <p>This is a story card.</p>
+      <p>ID: {id}</p> */}
       <p>Rating: {rating}/5</p>
       <p>Categories: {categories}</p>
-      <h6 className="mt-0 pb-3"> IMBD: <i className="fas fa-star" style={{color:'yellow'}}></i> <i className="fas fa-star" style={{color:'yellow'}}>
+      {/* <h6 className="mt-0 pb-3"> IMBD: <i className="fas fa-star" style={{color:'yellow'}}></i> <i className="fas fa-star" style={{color:'yellow'}}>
 
-      </i> <i className="fas fa-star" style={{color:'yellow'}}></i></h6>
+      </i> <i className="fas fa-star" style={{color:'yellow'}}></i></h6> */}
       <Link to= {`/stories/${slug}`}>
         <Button className="btn-danger">Read More</Button>
       </Link>
--- client/src/components/BrowsingGrid/StoryCards/StoryCards.js
+++ client/src/components/BrowsingGrid/StoryCards/StoryCards.js
@@ -22,46 +22,14 @@ const responsive = {
   }
 };
 
+
 export default function StoryCards() {
 
-  const [storiesList, setStoriesList]= useState([]);
-
-  useEffect(() => {
-    async function fetchData () {
-      try {
-        const {data} = await instance.get('/stories');
-        console.log(data);
-        setStoriesList(data.all);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  },[]);
-
-  const chosenStoriesCards = storiesList.map(story =>
-    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} imgUrl={story.imgUrl} rating={story.rating} slug={story.slug} categories={story.categories+''} />
-  );
-
-  return (
-    <>
-      <h2 style={{color:'#cc8e35'}} >All Stories:</h2>
-      <Carousel responsive={responsive} centerMode={true} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
-        {chosenStoriesCards}
-      </Carousel>
-      </>
-  )
-}
-
-/* export default function StoryCards() {
-
-  const [storiesList, setStoriesList]= useState([]);
-  const [kidsList, setKidsList]=useState([]);
-  const [actionList, setactionList]=useState([]);
-  const [cartoonList, setcartoonList]=useState([]);
-  const [fantasylist, setfantasyList]=useState([]);
-  const [animationList, setanimationList]=useState([]);
-  const [comedyList, setcomedyList]=useState([]);
+  const [allList, setAllList]= useState([]);
+  const [mysteryList, setMysteryList]=useState([]);
+  const [eroticList, setEroticList]=useState([]);
+  const [romanceList, setRomanceList]=useState([]);
+  const [scifilist, setScifiList]=useState([]);
 
   const [top10List, setTop10List] = useState([]);
   const [popularInAreaList, setPopularInAreaList] = useState([]);
@@ -72,38 +40,44 @@ export default function StoryCards() {
       try {
         const {data} = await instance.get('/stories');
         console.log(data);
-        const allData= data.all;
-        setStoriesList(allData);
-        setKidsList(data.kids);
-        setactionList(data.action);
-        setcartoonList(data.cartoon);
-        setfantasyList(data.fantasy);
-        setanimationList(data.animation);
-        setcomedyList(data.comedy);
+        setAllList(data.all);
+        setMysteryList(data.mystery);
+        setEroticList(data.erotic);
+        setRomanceList(data.romance);
+        setScifiList(data.scifi);
 
         // random, hard-coded stories to be rendered in top10
         const top10Stories = [];
         // add the stories you want to appear in top10
         top10Stories.push(data.all[4]);
-        top10Stories.push(data.all[2]);
+        top10Stories.push(data.all[14]);
+        top10Stories.push(data.all[58]);
+        top10Stories.push(data.all[43]);
+        top10Stories.push(data.all[29]);
         top10Stories.push(data.all[0]);
-        top10Stories.push(data.all[3]);
+        top10Stories.push(data.all[35]);
+        top10Stories.push(data.all[41]);
+        top10Stories.push(data.all[55]);
+        top10Stories.push(data.all[20]);
         setTop10List(top10Stories);
 
         // random, hard-coded stories to be rendered in "popular in your area"
         const popularInAreaStories = [];
         // add the stories you want to appear in "popular in your area"
-        popularInAreaStories.push(data.all[1]);
-        popularInAreaStories.push(data.all[3]);
-        popularInAreaStories.push(data.all[0]);
+        popularInAreaStories.push(data.all[51]);
+        popularInAreaStories.push(data.all[43]);
+        popularInAreaStories.push(data.all[14]);
+        popularInAreaStories.push(data.all[29]);
+        popularInAreaStories.push(data.all[36]);
         setPopularInAreaList(popularInAreaStories);
 
         // random, hard-coded stories to be rendered in "recently added"
         const recentStories = [];
         // add the stories you want to appear in "recently added"
-        recentStories.push(data.all[2]);
-        recentStories.push(data.all[1]);
-        recentStories.push(data.all[4]);
+        recentStories.push(data.all[0]);
+        recentStories.push(data.all[11]);
+        recentStories.push(data.all[42]);
+        recentStories.push(data.all[3]);
         setRecentList(recentStories);
       } catch (err) {
         console.log(err);
@@ -115,100 +89,84 @@ export default function StoryCards() {
   //console.log(category)
   //console.log(setactionList);
   const top10Category = top10List.map(story =>
-    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} rating={story.rating} slug={story.slug} categories={story.categories+''} />
+    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} imgUrl={story.imgUrl} rating={story.rating} slug={story.slug} categories={story.categories+''} />
   );
 
   const popularInAreaCategory = popularInAreaList.map(story =>
-    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} rating={story.rating} slug={story.slug} categories={story.categories+''} />
+    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} imgUrl={story.imgUrl} rating={story.rating} slug={story.slug} categories={story.categories+''} />
   );
 
   const recentCategory = recentList.map(story =>
-    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} rating={story.rating} slug={story.slug} categories={story.categories+''} />
+    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} imgUrl={story.imgUrl} rating={story.rating} slug={story.slug} categories={story.categories+''} />
   );
 
-  const chosenStoriesCards = storiesList.map(story =>
-    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} rating={story.rating} slug={story.slug} categories={story.categories+''} />
+  const allListCategory = allList.map(story =>
+    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} imgUrl={story.imgUrl} rating={story.rating} slug={story.slug} categories={story.categories+''} />
   );
 
-  const kidsListCategory = kidsList.map(story =>
-    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} rating={story.rating} slug={story.slug} categories={story.categories+''} />
+  const mysteryListCategory = mysteryList.map(story =>
+    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} imgUrl={story.imgUrl} rating={story.rating} slug={story.slug} categories={story.categories+''} />
   );
 
-  const actionListCategory = actionList.map(story =>
-    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} rating={story.rating} slug={story.slug} categories={story.categories+''} />
+  const eroticListCategory = eroticList.map(story =>
+    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} imgUrl={story.imgUrl} rating={story.rating} slug={story.slug} categories={story.categories+''} />
+  );
+  
+    const romanceListCategory = romanceList.map(story =>
+      <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} imgUrl={story.imgUrl} rating={story.rating} slug={story.slug} categories={story.categories+''} />
+    );
+
+  const scifiListCategory = scifilist.map(story =>
+    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} imgUrl={story.imgUrl} rating={story.rating} slug={story.slug} categories={story.categories+''} />
   );
 
-  const fantasyListCategory = fantasylist.map(story =>
-    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} rating={story.rating} slug={story.slug} categories={story.categories+''} />
-  );
-
-  const cartoonListCategory = cartoonList.map(story =>
-    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} rating={story.rating} slug={story.slug} categories={story.categories+''} />
-  );
-
-  const animationListCategory = animationList.map(story =>
-    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} rating={story.rating} slug={story.slug} categories={story.categories+''} />
-  );
-
-  const comedyListCategory = comedyList.map(story =>
-    <StoryCard key={story._id} id={story._id} title={story.title} author={story.author} rating={story.rating} slug={story.slug} categories={story.categories+''} />
-  );
 
   return (
     <>
       <h2 style={{color:'#cc8e35'}} >Top 10:</h2>
-      <Carousel responsive={responsive} centerMode={true} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
+      <Carousel responsive={responsive} draggable={false} /* infinite={true}  */removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
         {top10Category}
       </Carousel>
 
       <h2 style={{color:'#cc8e35'}} >Popular in your area:</h2>
-      <Carousel responsive={responsive} centerMode={true} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
+      <Carousel responsive={responsive} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
         {popularInAreaCategory}
       </Carousel>
 
       <h2 style={{color:'#cc8e35'}} >Recently added stories:</h2>
-      <Carousel responsive={responsive} centerMode={true} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
+      <Carousel responsive={responsive} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
         {recentCategory}
       </Carousel>
 
       <h2 style={{color:'#cc8e35'}} >All Stories:</h2>
-      <Carousel responsive={responsive} centerMode={true} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
-        {chosenStoriesCards}
+      <Carousel responsive={responsive} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
+        {allListCategory}
       </Carousel>
 
-      <h2 style={{color:'#cc8e35'}}>Kids:</h2>
-      <Carousel responsive={responsive} centerMode={true} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
-        {kidsListCategory}
+      <h2 style={{color:'#cc8e35'}}>Mystery/Crime:</h2>
+      <Carousel responsive={responsive} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
+        {mysteryListCategory}
       </Carousel>
 
-      <h2 style={{color:'#cc8e35'}}>Actions:</h2>
-      <Carousel responsive={responsive} centerMode={true} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
-        {actionListCategory}
+      <h2 style={{color:'#cc8e35'}}>Erotic:</h2>
+      <Carousel responsive={responsive} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
+        {eroticListCategory}
       </Carousel>
 
-      <h2 style={{color:'#cc8e35'}}>fantasy:</h2>
-      <Carousel responsive={responsive} centerMode={true} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
-        {fantasyListCategory}
+      <h2 style={{color:'#cc8e35'}}>Romance:</h2>
+      <Carousel responsive={responsive} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
+        {romanceListCategory}
       </Carousel>
 
-      <h2 style={{color:'#cc8e35'}}>Cartoons:</h2>
-      <Carousel responsive={responsive} centerMode={true} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
-        {cartoonListCategory}
-      </Carousel>
-
-      <h2 style={{color:'#cc8e35'}}>Animations:</h2>
-      <Carousel responsive={responsive} centerMode={true} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
-        {animationListCategory}
-      </Carousel>
-
-      <h2 style={{color:'#cc8e35'}}>Comedy:</h2>
-      <Carousel responsive={responsive} centerMode={true} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
-        {comedyListCategory}
+      <h2 style={{color:'#cc8e35'}}>Science Fiction:</h2>
+      <Carousel responsive={responsive} draggable={false} infinite={true} removeArrowOnDeviceType={['mobile']} containerClass="carousel-container">
+        {scifiListCategory}
       </Carousel>
     </>
     
   );
-} */
+}
 
 // draggable=false disables dragging on desktop
 // infinite=true makes it infinite, it does NOT jump back to the beginning when clicking "next" at the last item
+// centerMode={true} shows a bit of the next/prev cards on the side of the screen
