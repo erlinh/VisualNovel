2e89aa0ec9f100b553f102a74dc8172d3627c75f
kata-v <7273558+kata-v@users.noreply.github.com>
2020-11-19 17:04:41 +0200

    Recreated the text settings menu

--- client/src/components/TextView/TextSettings/TextSettings.js
+++ client/src/components/TextView/TextSettings/TextSettings.js
@@ -1,29 +1,67 @@
 import React, { useState } from 'react';
 import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Collapse, Button } from 'reactstrap';
 import {Link} from 'react-router-dom';
 import './TextSettings.css';
 
 const TextSettings = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   const [dropdownOpen, setOpen] = useState(false);
 
-  const toggle = () => setOpen(!dropdownOpen);
+  const toggleDropDown = () => setOpen(!dropdownOpen);
 
   return (
-    <ButtonDropdown isOpen={dropdownOpen} toggle={toggle} className="textSettings">
-      <DropdownToggle caret>
-              
-      </DropdownToggle>
-      <DropdownMenu>
-        <DropdownItem header>
-                  Setting go here
-        </DropdownItem>
-        <DropdownItem> 
-          <Link to="/">Home</Link>
-        </DropdownItem>
-      </DropdownMenu>
+    <div>
+      <Button color="light" onClick={toggle} className="settingsBtn"><i className="fas fa-cog"></i></Button>
+      <Collapse isOpen={isOpen}>
+
+       <div className="textSettingsContainer">
+         
+
+         {/* This dropdown menu is intended for the reader to adjust the page view into their liking */}
+         <ButtonDropdown isOpen={dropdownOpen} toggle={toggleDropDown}>
+            <DropdownToggle color="light">
+                    Aa
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>
+                <p>Text modifiers</p>
+              </DropdownItem>
+              <DropdownItem> 
+                <p>Text Size</p>
+              </DropdownItem>
+              <DropdownItem> 
+                <p>Color</p>
+              </DropdownItem>
+              <DropdownItem> 
+                <p>Font</p>
+              </DropdownItem>
+              <DropdownItem> 
+                <p>Margins</p>
+              </DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+
+{/* A search function could be implemented here. Kindle has this feature. */}
+         <Button color="light"><i class="fas fa-search"></i></Button>    
+
+{/* Functionality to add a bookmark on a page could be implemented here. Kindle has this feature. */}
+         <Button color="light"><i class="fas fa-bookmark"></i></Button>        
+
+         <Link to="/">Home</Link>
+
+       </div>
+      </Collapse>
+    </div>
 
 
-    </ButtonDropdown>
+
+ 
+    
     
   );
 };
