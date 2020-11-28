9e99d0c4dfc9c2e8818ed21353ae90d2b8aa3a64
kata-v <7273558+kata-v@users.noreply.github.com>
2020-11-28 20:09:48 +0200

    Mock-up of overlay details

--- client/src/components/BrowsingGrid/StoryCard/StoryCard.js
+++ client/src/components/BrowsingGrid/StoryCard/StoryCard.js
@@ -7,10 +7,19 @@ import {
 
 import './StoryCard.css';
 
+const overlayOn = () => {
+  document.getElementById("overlay").style.display = "grid";
+}
+
+const overlayOff = () => {
+  document.getElementById("overlay").style.display = "none";
+}
+
 const StoryCard=({id, title, author, imgUrl, rating, slug, categories})=> {
   return (
     <Card style={{width: '300px', height:'460px', backgroundColor:'black', borderRadius:'5px', border:'1px #1e272e solid', lineHeight:'1.2'}} 
       className="p-2 mr-5 text-light">
+      <Button onClick={overlayOn}>Show overlay</Button>
       <img className="pb-3" src={imgUrl} alt="Cover of the story" />
       <CardTitle style={{fontSize:'2rem'}}>{title}</CardTitle>
       <CardSubtitle>By {author}</CardSubtitle>
@@ -18,12 +27,14 @@ const StoryCard=({id, title, author, imgUrl, rating, slug, categories})=> {
       <p>ID: {id}</p> */}
       <p>Rating: {rating}/5</p>
       <p>Categories: {categories}</p>
-      {/* <h6 className="mt-0 pb-3"> IMBD: <i className="fas fa-star" style={{color:'yellow'}}></i> <i className="fas fa-star" style={{color:'yellow'}}>
-
-      </i> <i className="fas fa-star" style={{color:'yellow'}}></i></h6> */}
+     
       <Link to= {`/stories/${slug}`}>
         <Button className="getDetails">Get Details</Button>
       </Link>
+      <div id="overlay">
+        <p>This is overlay</p>
+        <Button className="btnClose" onClick={overlayOff}>Close</Button>
+      </div>
      
     </Card>
   );
