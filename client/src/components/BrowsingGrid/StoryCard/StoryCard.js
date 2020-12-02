4dfb4aac088e76754724b489aa23fb146b352c86
kata-v <7273558+kata-v@users.noreply.github.com>
2020-12-02 09:03:33 +0200

    Adjusted story card size to be responsive

--- client/src/components/BrowsingGrid/StoryCard/StoryCard.js
+++ client/src/components/BrowsingGrid/StoryCard/StoryCard.js
@@ -8,7 +8,7 @@ import {
 import './StoryCard.css';
 
 const overlayOn = (id) => {
-  document.getElementById(id).style.display = "grid";
+  document.getElementById(id).style.display = "flex";
 }
 
 const overlayOff = (id) => {
@@ -17,22 +17,25 @@ const overlayOff = (id) => {
 
 const StoryCard=({id, title, author, imgUrl, rating, slug, categories})=> {
   return (
-    <Card style={{width: '300px', height:'460px', backgroundColor:'black', borderRadius:'5px', border:'1px #1e272e solid', lineHeight:'1.2'}} 
+    <Card style={{backgroundColor:'black', borderRadius:'5px', border:'1px #1e272e solid', lineHeight:'1.2', position:'relative', zIndex:'-1'}} 
       className="p-2 mr-5 text-light">
       <img className="pb-3" src={imgUrl} alt="Cover of the story" onClick={() => overlayOn(id)}/>
      
-      <div id={id} onClick={() => overlayOff(id)} className="overlay">
-        <img className="overlayImg" src={imgUrl} alt="Cover of the story"/>         
-        <CardTitle style={{fontSize:'2rem'}}>{title}</CardTitle>
-        <CardSubtitle>By {author}</CardSubtitle>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis elementum nulla ac pulvinar. Proin facilisis malesuada erat, sit amet semper mauris finibus id. Vivamus vitae ex quis nulla viverra.</p>
-        <p>Rating: {rating}/5</p>        
-        <p>Categories: {categories}</p>
-      
-        <Link to= {`/stories/${slug}`}>
-          <Button className="getDetails">Get Details</Button>
-        </Link>
+      <div id={id} onClick={() => overlayOff(id)} className="overlay" style={{zIndex:'10'}}>
+        <div className="overlayContent">
+        <img className="overlayImg" src={imgUrl} alt="Cover of the story"/> 
+        <div className="overlayText">       
+          <CardTitle style={{fontSize:'2rem'}}>{title}</CardTitle>
+          <CardSubtitle>By {author}</CardSubtitle>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis elementum nulla ac pulvinar. Proin facilisis malesuada erat, sit amet semper mauris finibus id. Vivamus vitae ex quis nulla viverra.</p>
+          <p>Rating: {rating}/5</p>        
+          <p>Categories: {categories}</p>
         
+          <Link to= {`/stories/${slug}`}>
+            <Button className="getDetails">Get Details</Button>
+          </Link>
+        </div>
+        </div>
       </div>
      
     </Card>
