67e520f4056faed11be7a88105c5d8cf37a2357d
bea94k <beatakusnierz94@gmail.com>
2020-12-04 10:52:18 +0200

    Change ProtectedRoute to use useContext instead of Consumer

--- client/src/firebase/ProtectedRoute.js
+++ client/src/firebase/ProtectedRoute.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { AuthConsumer } from './authContext';
+import React, { useContext } from 'react';
+import { AuthContext } from './authContext';
 import { Route, Redirect} from 'react-router-dom';
 
 
-const ProtectedRoute = ({ component: Component, ...rest }) => (
-    <AuthConsumer>
-      {({ user }) => (
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+    const { user } = useContext(AuthContext);
+
+    return (
         <Route render={props =>
             user ? <Component {...props} /> : <Redirect to="/login" /> }
           {...rest}
         />
-      )}
-    </AuthConsumer>
-  );
+    )
+};
 
   export default ProtectedRoute;
