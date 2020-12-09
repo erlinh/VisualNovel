568417c64c404f8980c9a127ec0dfd2ed49a70de
bea94k <beatakusnierz94@gmail.com>
2020-12-09 11:21:48 +0200

    Text scroll, choices buttons in the last screen

--- client/src/components/TextView/TextContent/TextChunk.js
+++ client/src/components/TextView/TextContent/TextChunk.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './TextContent.css';
 
-const TextChunk = ({id, content, toggleReadingNavOpen, textSizeClass, bgColorClass, fontClass, marginsClass, spacingClass}) => {
+const TextChunk = ({content, toggleReadingNavOpen, textSizeClass, bgColorClass, fontClass, marginsClass, spacingClass}) => {
   return (
-    <div onClick={toggleReadingNavOpen} className={`text-columns text-size-${textSizeClass} font-${fontClass} bg-${bgColorClass} margins-${marginsClass} spacing-${spacingClass}`}>
-      {/* <p>Page {id}</p> */}
-      <p className="mb-0">{content}</p>
+    <div onClick={toggleReadingNavOpen} className={`text-view-grid text-size-${textSizeClass} font-${fontClass} bg-${bgColorClass} margins-${marginsClass} spacing-${spacingClass}`}>
+
+      <div className="text-grid">
+        <p className="mb-0 text-columns">{content}</p>
+      </div>
+
+      <div className="choice-screen">
+        <h3>Choice buttons here</h3>
+      </div>
+
     </div>
   );
 };
--- client/src/components/TextView/TextContent/TextContent.js
+++ client/src/components/TextView/TextContent/TextContent.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-//import Carousel from 'react-multi-carousel';
-// import 'react-multi-carousel/lib/styles.css';
 import instance from '../../../axios'; 
 import TextChunk from './TextChunk';
 import './TextContent.css';
@@ -30,65 +28,10 @@ const TextContent = ({toggleReadingNavOpen, textSizeClass, bgColor, fontClass, m
       { contents.length === 0 ? (
         <h3>Loading...</h3>
       ) : (
-        <div className="text-view-grid">
           <TextChunk content={contents[currentChunkId].text} toggleReadingNavOpen={toggleReadingNavOpen} textSizeClass={textSizeClass} bgColorClass={bgColor} fontClass={fontClass} marginsClass={marginsClass} spacingClass={spacingClass} />
-          <div className="choice-screen">
-            <h3>Choice buttons here</h3>
-          </div>
-        </div>
       )}
     </div>
   );
 };
 
 export default TextContent;
-
-
-/* const TextContent = ({toggleReadingNavOpen, textSizeClass, fontClass, marginsClass, spacingClass}) => {
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 2,
-      slidesToSlide: 2
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      slidesToSlide: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
-  const [contents, setContents]=useState([]);
-  const slug= useParams().slug;
-
-  useEffect(()=>{
-    async function fetchContent(){
-      try {
-        const {data} = await instance.get(`/stories/${slug}/content`);
-        setContents(data.content);
-      } catch (err) {
-        console.log('There is an error to get content', err);
-      }
-    }
-    fetchContent();
-  },[slug]);
-
-  const pagesFromDatabase = contents.map((page) => (
-    <TextCard key={page.id} content={page.text} toggleReadingNavOpen={toggleReadingNavOpen} textSizeClass={textSizeClass} fontClass={fontClass} marginsClass={marginsClass} spacingClass={spacingClass} />
-  ));
-
-  return (
-    <div className="textContentContainer">
-   
-      <Carousel responsive={responsive} removeArrowOnDeviceType={['mobile']}>
-        {pagesFromDatabase}
-      </Carousel>
-    </div>
-  );
-};
-
-export default TextContent;
- */
